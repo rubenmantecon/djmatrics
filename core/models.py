98b7c48b36e2bef44fbb7a8bd35f59e96b895222
Rubén M <dev.mantecon@gmail.com>
2021-04-26 18:22:30 +0200

    perf: Update UF and MP

--- core/models.py
+++ core/models.py
@@ -70,19 +70,20 @@ class MP(models.Model):
         verbose_name_plural = "MPs"
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=20)
-    desc = models.TextField(blank=True,null=True)
-    career = models.ForeignKey(Career,on_delete=models.CASCADE)
+    desc = models.TextField(blank=True, null=True)
+    career = models.ForeignKey(Career, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
+
 
 class UF(models.Model):
     class Meta:
         verbose_name_plural = "UFs"
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=20)
-    desc = models.TextField(blank=True,null=True)
-    mp = models.ForeignKey(MP,on_delete=models.CASCADE)
+    desc = models.TextField(blank=True, null=True)
+    mp = models.ForeignKey(MP, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-
-
