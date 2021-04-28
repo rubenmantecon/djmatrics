a9f5609837877be530741e162b40fe1cbedd7670
Rubén M <dev.mantecon@gmail.com>
2021-04-28 18:20:25 +0200

    feat:Configure settings to work with .env

--- djmatrics/settings.py
+++ djmatrics/settings.py
@@ -11,22 +11,38 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# environ object instatiation
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True)
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xoqhirlqtdrxohy&olf@y$o#))e%133$@k5t3$blxtj&%2u6s1'
+# reading .env file
+environ.Env.read_env()
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# False if not in os.environ
+DEBUG = env('DEBUG')
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+# Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
+DATABASES = {
+    # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
+    'default': env.db(),
+    # read os.environ['SQLITE_URL']
+    'extra': env.db('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
+}
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,21 +85,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djmatrics.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'matrics',
-        'USER': 'ruben',
-        'PASSWORD': 'ruben',
-        'HOST': '127.0.0.1'
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
