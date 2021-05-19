54ac41325be6618041053b02b72d84afdee49c76
Rubén M <dev.mantecon@gmail.com>
2021-05-19 16:36:14 +0200

    add: first working draft of calculatePrice()

--- core/functions.py
+++ core/functions.py
@@ -1,24 +1,27 @@
 from core.models import *
 import datetime
 
-public_price = 360
-material = 70
-insurance = 1.12
 
-enrolment = Enrolment.objects.get(dni='12345678Z')
-ufs_of_enrolment = UF.objects.filter(
-    term=enrolment.term)
-ufs_prices = sum([x.price for x in ufs_of_enrolment])
+def calculatePrice(matricula: Enrolment):
+    PUBLIC_PRICE = 360
+    UF_PRICE
+    MATERIAL = 70
+    insurance = 1.12
 
-age = int((datetime.date.today() - enrolment.birthday).total_seconds() / 31557600)
+    ufs_prices = matricula.uf.all().count() *
 
+    age = int((datetime.date.today() -
+              matricula.birthday).total_seconds() / 31557600)
 
-if age > 28:
-    insurance = 20
+    if age > 28:
+        insurance = 20
 
-if ufs_prices > public_price:
-    final_price = public_price + material + insurance
-else:
-    final_price = ufs_prices + material + insurance
-print("Calculated age:", age)
-print("Calculated price:", final_price)
+    if ufs_prices > PUBLIC_PRICE:
+        final_price = public_price + material + insurance
+        return final_price
+    else:
+        final_price = ufs_prices + material + insurance
+        return final_price
+
+matricula = Enrolment.objects.first()
+calculatePrice(matricula)
